46e440b433b0ec10b3af74649cc0968bd828f906
Josh Holman <git@jolman.me>
2026-06-02 12:53:29 -0700

    write worker

--- src/index.js
+++ src/index.js
@@ -10,6 +10,38 @@
 
 export default {
 	async fetch(request, env, ctx) {
-		return new Response("Hello World!");
+		const url = new URL(request.url);
+
+		const host = url.host;
+		const pathParts = url.pathname.split('/').filter(Boolean);
+
+		const githubAccount = "https://github.com/thejolman";
+		if (pathParts.length === 0) {
+			return Response.redirect(githubAccount, 302);
+		}
+
+		const repoName = pathParts[0];
+		const vanityPath = `${host}/${repoName}`;
+		const repoUrl = `${githubAccount}/${repoName}`;
+		const vcs = "git";
+
+		if (url.searchParams.get("go-get") === "1") {
+			const html = `
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="go-import" content="${vanityPath} ${vcs} ${repoUrl}">
+</head>
+<body>
+	go get ${vanityPath}
+</body>
+</html>
+`;
+			return new Response(html, {
+				headers: { "content-type": "text/html;charset=UTF-8" },
+			});
+		}
+
+		return Response.redirect(repoUrl, 302);
 	},
 };
